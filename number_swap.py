1ba077c7a80451c255bdf9a61bfa834292b33d82
aadyalithesh-coder <aadyalithesh@gmail.com>
2026-03-29 15:03:53 +0200

    Add files via upload

--- number_swap.py
+++ number_swap.py
@@ -1,9 +1,14 @@
 num1=89
 num2=73
 num3=28
-
+print("original num1:",num1)
+print("original num2:",num2)
+print("original num3:",num3)
 
 num1=num2
 num2=num3
 num3=num1
 
+print("changed num1:",num1)
+print("changed num2:",num2)
+print("changed num3:",num3)
